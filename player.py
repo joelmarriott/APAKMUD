f0768a32a59c6b755a8a08fca13760a994e137b4
joelmarriott <37147533+joelmarriott@users.noreply.github.com>
2018-03-08 13:54:54 +0000

    Created base player

---
+++ player.py
@@ -0,0 +1,8 @@
+class warrior:
+  race = "Human"
+  type = "Warrior"
+  def __init__(self, health, stamina, bag, location):
+    self.health = health
+    self.stamina = stamina
+    self.bag = bag
+    self.location = location
