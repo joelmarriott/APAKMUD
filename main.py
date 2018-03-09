fde89fd4d708d48677c0e6420d746a085f30ba44
joelmarriott <37147533+joelmarriott@users.noreply.github.com>
2018-03-09 13:19:07 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,54 @@
-from player import warrior
+import player
 
-while 1:
-  difficulty = raw_input("Welcome, please choose difficulty [Easy/Hard/Insane]: ")
-  difficulty = difficulty.upper()
-  
-  if difficulty == 'EASY':
-    health, stamina = 100, 100
-    bag = {'Potions':10}
-  elif difficulty == 'HARD':
-    health, stamina = 50, 50
-    bag = {'Potions':5}
-  elif difficulty == 'INSANE':
-    health, stamina = 25, 25
-    bag = {'Potions':2}
-  else:
-      print("Input incorrect")
-      
-  location = "outside"
-  p1 = warrior(health, stamina, bag, location)
-  
-  print("Health: {}, Stamina: {}. You are located {}").format(p1.health, p1.stamina, p1.location)
-  print("Bag:")
-  for a, b in p1.bag.items():
-    print("{} of {}").format(b, a)
+def outside(player):
+    where = raw_input('Go back to car, or enter the building? (Car/Enter) : ')
+    where = where.upper()
+
+    if where == 'CAR':           ## Make a car location
+        print("You rummage around in your car, and find your keyfob. Oops!")
+        player.bag['Keyfob'] = 1
+        player.location = reception
+    print 'End of room (Health, Stamina, Potions): ', health, stamina, potions
+    return player
+
+def reception(player):
+    if 'Keyfob' in player.bag:
+        print "You walk into reception"
+        player.location = hallway
+    else:
+        print "You forgot your keyfob..."
+        player.health = 0
+    return player
+
+def selDiff():
+    "Return health, stamina and potions based on difficulty selection"
+    while 1:
+        difficulty = raw_input('Please choose difficulty, (\033[92mEasy, \033[93mHard, \033[91mInsane\033[0m): ')
+        difficulty = difficulty.upper()
+        if difficulty == 'EASY':
+            health = 100
+            stamina = 100
+            potions = 10
+            break
+        elif difficulty == 'HARD':
+            health = 50
+            stamina = 50
+            potions = 5
+            break
+        elif difficulty == 'INSANE':
+            health = 10
+            stamina = 10
+            potions = 1
+            break
+        else:
+            print("Input invalid.")
+    return health, stamina, potions
+
+if __name__ == '__main__':
+    deaths = 0
+    while 1:
+        
+        health, stamina, potions = selDiff()
+        p1 = Player(health, stamina, potions)
+        while health > 0:
+            p1 = p1.location(p1)
